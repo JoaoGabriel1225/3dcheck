61b98446d8138e3eb83a1a5bcca08f9d1d55a503
João Gabriel Pires Tardeli <joaogabrielpires11@gmail.com>
2026-04-30 17:53:16 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,8 @@
 // public/sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
 
-// Nome do cache (pode mudar quando atualizar o app)
-const CACHE_NAME = '3dcheck-v1';
-
-// Evento de Instalação
-self.addEventListener('install', (event) => {
-  console.log('SW: Instalado');
-});
-
-// Evento de Ativação
-self.addEventListener('activate', (event) => {
-  console.log('SW: Ativado');
-});
-
-// Evento de Fetch (Obrigatório para ser instalável)
 self.addEventListener('fetch', (event) => {
-  // Por enquanto, apenas deixa as requisições passarem normalmente
-  event.respondWith(fetch(event.request));
+  // Apenas ignora e deixa o navegador seguir o fluxo normal
+  return;
 });
